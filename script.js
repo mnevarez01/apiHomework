66c5806e454f87574500945f17f64f7c346d5dab
Maria Nevarez <mnevarez1220@gmail.com>
2020-03-30 21:06:03 -0600

    updated sheets

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 //create all variables from the HTML that you will manipulate
-var containerEl = document.querySelector("#modal-container");
-var timerEL = document.querySelector("#time");
+var currentQuestionIndex = 0;
+var time = 60;
 var score = 0;
-var timeLeft = 60; //how to count down time?
+var timerId;
+
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("startBtn");
+var initialsEL = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");
+
 //OBJECT----create the questions, answer options and correct answer
 var listQuestions = [
   {
@@ -13,90 +22,74 @@ var listQuestions = [
       "font-style",
       "text-decoration"
     ],
-    answer: 2
+    answer: "color"
   },
   {
     questions: "What is NOT a Data Type?",
     answerOptions: ["Boolean", "Number", "function", "String"],
-    answer: 3
+    answer: "function"
   },
   {
     questions: "How do you link your JavaScript?",
     answerOptions: ["Script>", "Link>", "h1>", "body>"],
-    answer: 1
+    answer: "Script>"
   },
   {
     questions: "What is used in HTML?",
     answerOptions: ["<body>", "<header>", "<a>", "All of the above"],
-    answer: 4
+    answer: "All of the above"
   },
   {
     questions: "What do you use to group together an Array?",
     answerOptions: ['" "', "{ }", "[ ]", "< >"],
-    answer: 3
+    answer: "[ ]"
   }
 ];
 
-var questionIndex = listQuestions.questions;
-var answer = listQuestions.answerOptions;
-
-//create questionIndex with all the right questions-use this to compare correct answer
-//prompt the questions create a loop to go through the object and ask the questions
-//answers need to be stored into an array this would go into the userArray
-//create buttons to match answer key
-//add event listener to the button to signify the answers
-//create userAnswers-array
-function generateQuiz() {
-  var userAnswers = [];
-  var correctAnswers = listQuestions.answer[i];
-  for (var i = 0; i < questions.length; i++) {
-    correctAnswers = [];
-  }
+function startQuiz(event) {
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
+  questionsEl.removeAttribute("class");
+  timerId = setInterval(clockTick, 1000);
+  timerEl.textContent = time;
+  startQuestions();
 }
 
-//score keeping
-//display score---GAME IS OVER!!
-//make sure to add eventdelegation!!
-//save the results and put initials-this involve localStorage
-function showResults() {
-  if (listQuestions[i].answer) {
-    score++;
-  } else {
-    score--;
-    timeLeft - 10;
-  }
+function startQuestions() {
+  var currentQuestion = questions[currentQuestionIndex];
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = listQuestions[i].questions;
+  choicesEl.innerHTML = "";
 }
 
-//counting down - when it hits zero stop the quiz set time = 0;
-//wrong answer subtracts ten seconds from allotted time
-function timer() {
-  var timerInt = setInterval(function() {
-    timeLeft--;
-    timerEl.textContent = timeLeft + ": second left!";
-
-    if (timeleft === 0) {
-      clearInterval(timerInt);
-      showResults();
+function answerOptions() {
+  for (i = 0; i < questions.length; i++) {
+    var correctAnswers = listQuestions.answer;
+    if (userAnswers === answerOptions[i]) {
+      score++;
+    } else {
+      score--;
+      time - 10;
+      alert("not quite right!");
     }
-  }, 1000);
-}
-// function renderQuestions(){
-
-for (var i = 0; i < questionIndex.length; i++) {
-  var userAnswers = confirm(questionIndex[i]);
-  if (userAnswers === correctAnswer[i]) {
-    score++;
-  } else {
-    score--;
-    time--;
   }
 }
-// }
 
-//start timer to count down function to call on when the click of image goes away.
-//MAYBE have a picture first then make it go away when they click start ---event listener
-function imageDisplay() {
-  containerEl.style.display = "none";
+function finishQuiz() {
+  clearInterval(timerId);
+  var startScreenEl;
+  startScreenEl.removeAttribute("class");
+  var finalScore;
+  finalScoreEl.textContent.time = time;
+  questionsEl.setAttribute("class", "hide");
 }
-//create start button
-container.addEventListener("click", imageDisplay);
+
+function countDown() {
+  time--;
+  timerEl.textContent = time;
+  if (time <= 0) {
+    finnishQuiz();
+  }
+}
+
+startBtn.addEventListener("click", startQuiz);
